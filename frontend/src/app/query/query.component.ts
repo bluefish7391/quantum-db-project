82ef49c21f554ba07ecb99db9b9185bd2c4c30cc
Brian Nguyen <bluefish7391@gmail.com>
2026-01-19 15:40:48 -0500

    feat (query): create basic database viewing and updating page template

--- frontend/src/app/query/query.component.ts
+++ frontend/src/app/query/query.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { User } from '../../kinds';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-query',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './query.component.html',
   styleUrl: './query.component.scss',
 })
 export class QueryComponent {
+  users: User[] = [];
+  newUser: User = new User();
+  constructor (
 
+  ) {}
+
+  editUser(user: User) {
+    // to implement
+  }
+
+  deleteUser(id: number) {
+    // to implement
+  }
+
+  addUser() {
+    // to implement
+  }
+
+  loadSampleDatabase() {
+    // to implement
+  }
 }
