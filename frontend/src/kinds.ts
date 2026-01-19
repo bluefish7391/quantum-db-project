f58cc99685df90e5a4358b36874f57b18ded3090
Brian Nguyen <bluefish7391@gmail.com>
2026-01-19 15:41:56 -0500

    feat (kinds): create user class

---
+++ frontend/src/kinds.ts
@@ -0,0 +1,12 @@
+
+export class User {
+    id: number;
+    name: string;
+    phone: string;
+
+    constructor(id: number = 0, name: string = '', phone: string = '') {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
