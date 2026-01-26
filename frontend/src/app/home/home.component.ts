3651c04e009a028fed3c3451334202f81ea46ba7
Brian Nguyen <bluefish7391@gmail.com>
2026-01-26 15:29:54 -0500

    feat (home): create home component template

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatButton,
+    MatCardSubtitle, MatDivider, MatIcon, MatCardActions, RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor() { }
 }
